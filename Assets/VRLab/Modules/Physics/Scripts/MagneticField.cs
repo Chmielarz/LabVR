bb971b5d97e2a7b18ecf40413b36f125182924b9
kwjay <kwjay1002@gmail.com>
2023-11-10 22:23:33 +0100

    Adjusted script for both repulsion and attraction

--- Assets/VRLab/Modules/Physics/Scripts/MagneticField.cs
+++ Assets/VRLab/Modules/Physics/Scripts/MagneticField.cs
@@ -4,15 +4,61 @@ using UnityEngine;
 
 public class MagneticField : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private bool repulsion = false;
+    private bool attraction = false;
+    private bool inContact = false;
+    private Vector3 otherObjectPosition;
+    private float distance;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (!inContact)
+        {
+            if (repulsion)
+            {
+                distance = Vector3.Distance(otherObjectPosition, transform.position);
+                transform.position = Vector3.MoveTowards(transform.position, otherObjectPosition, distance * Time.deltaTime * -1);
+            }
+            else if (attraction)
+            {
+                distance = Vector3.Distance(otherObjectPosition, transform.position);
+                transform.position = Vector3.MoveTowards(transform.position, otherObjectPosition, distance * Time.deltaTime);
+                Debug.Log("ATTRACTION");
+            }
+        }
         
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == gameObject.tag)
+        {
+            otherObjectPosition = other.transform.position;
+            repulsion = true;
+        }
+        else if (other.CompareTag("MagneticNorthPole") || other.CompareTag("MagneticSouthPole"))
+        {
+            otherObjectPosition = other.transform.position;
+            attraction = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        repulsion = false;
+        attraction = false;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        inContact = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        inContact = false;
     }
 }
